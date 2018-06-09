ff317d7310bd3346ff07f4af80a29d73d81a688e
Oktawia Kata <oktawia.kata@gmail.com>
2018-06-09 19:43:47 +0200

    Prepare static export

--- next.config.js
+++ next.config.js
@@ -1,2 +1,11 @@
 const withSass = require('@zeit/next-sass')
 module.exports = withSass()
+
+exports.exportPathMap = () => ({
+    "/": { page: "/" },
+    "/iscrizioni": { page: "/iscrizioni" },
+    "/regole": { page: "/regole" },
+    "/struttura": { page: "/struttura" },
+    "/sponsor": { page: "/sponsor" },
+    "/contatti": { page: "/contatti" },
+  })

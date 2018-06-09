73bc08108521cbf54b6fd925c88744a1b09a1785
Oktawia Kata <oktawia.kata@gmail.com>
2018-06-09 14:41:00 +0200

    Add styled components setup

--- pages/_document.js
+++ pages/_document.js
@@ -1,12 +1,32 @@
 import Document, { Head, Main, NextScript } from 'next/document'
+import { ServerStyleSheet, injectGlobal } from 'styled-components'
+
+injectGlobal`
+  body {
+    margin: 0;
+    padding: 0;
+    font-family: 'Jua', sans-serif;
+    font-size: 22px;
+  }
+`;
 
 export default class MyDocument extends Document {
+  static getInitialProps ({ renderPage }) {
+    const sheet = new ServerStyleSheet()
+    const page = renderPage(App => props => sheet.collectStyles(<App {...props} />))
+    const styleTags = sheet.getStyleElement()
+    return { ...page, styleTags } // return styles collected
+  }
+
   render() {
     return (
       <html>
         <Head>
-          <link rel="stylesheet" href="/_next/static/style.css" />
+          <title>Torneo città di Mejaniga</title>
+          <meta name="viewport" content="initial-scale=1.0, width=device-width" />
           <link href="https://fonts.googleapis.com/css?family=Jua" rel="stylesheet"/>
+          <link rel="stylesheet" href="/_next/static/style.css"/>
+          {this.props.styleTags}
         </Head>
         <body>
           <Main />

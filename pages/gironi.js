8beb45cb50f04170380bc4f553bfba97c5133902
aiwatko <oktawia.kata@gmail.com>
2018-07-07 00:39:37 +0200

    Add gironi template

---
+++ pages/gironi.js
@@ -0,0 +1,35 @@
+import React, { Component } from 'react'
+import styled from 'styled-components';
+
+import Layout from '../components/Layout'
+
+const Container = styled.div`
+    max-width: 1024px; 
+    margin: 0 auto; 
+`;
+
+const Title = styled.h1`
+    text-align: center;
+`;
+
+const Image = styled.img`
+    position: relative;
+    width: 100%;
+`;
+
+class Gironi extends Component {
+    render() {
+        return (
+            <Layout>
+                {
+                    <Container>
+                        <Title>Gironi</Title>    
+                        <Image src='static/gironi/gironi.jpeg' alt="Gironi"/>                    
+                    </Container>
+                }
+            </Layout>    
+        );
+    }
+  }
+  
+export default Gironi;

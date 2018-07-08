d55b721d025220321dc887095bcdd050ff9b1169
aiwatko <oktawia.kata@gmail.com>
2018-07-08 14:35:01 +0200

    Add pages

---
+++ pages/rose.jsx
@@ -0,0 +1,35 @@
+import React, { Component } from 'react'
+import styled from 'styled-components';
+
+import Layout from '../components/Layout'
+
+const Container = styled.div`
+    max-width: 1024px; 
+    margin: 0 auto; 
+`;
+
+const Title = styled.h1`
+    text-align: center;
+`;
+
+const Image = styled.img`
+    position: relative;
+    width: 100%;
+`;
+
+class Rose extends Component {
+    render() {
+        return (
+            <Layout>
+                {
+                    <Container>
+                        <Title>Rose</Title>    
+                        <Image src='/static/aggiornamenti/rose.jpeg' alt="Rose"/>                    
+                    </Container>
+                }
+            </Layout>    
+        );
+    }
+  }
+  
+export default Rose;

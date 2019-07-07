c7f43b6c80bdfd3e900c4fe63fb999e9fb78f812
aiwatko <oktawia.kata@gmail.com>
2019-07-07 19:40:16 +0200

    Remove sass

--- next.config.js
+++ next.config.js
@@ -1,11 +1,8 @@
-const withSass = require('@zeit/next-sass')
-module.exports = withSass()
-
 exports.exportPathMap = () => ({
-    "/": { page: "/" },
-    "/iscrizioni": { page: "/iscrizioni" },
-    "/regole": { page: "/regole" },
-    "/struttura": { page: "/struttura" },
-    "/sponsor": { page: "/sponsor" },
-    "/contatti": { page: "/contatti" },
-  })
+  '/': { page: '/' },
+  '/iscrizioni': { page: '/iscrizioni' },
+  '/regole': { page: '/regole' },
+  '/struttura': { page: '/struttura' },
+  '/sponsor': { page: '/sponsor' },
+  '/contatti': { page: '/contatti' }
+});
